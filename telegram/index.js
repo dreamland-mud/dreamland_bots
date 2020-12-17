b65e68681ec8f7a57437605da4b0d63da3f6dd9d
Zaleshanin <jbo@ua.fm>
2020-12-17 12:12:26 +0300

    Update index.js
    cat option fixed by adding parser function. more cat help added (может оно и не нужно).

--- telegram/index.js
+++ telegram/index.js
@@ -18,7 +18,15 @@ bot.help((ctx) => ctx.reply(
     '/cat says/meow meow - кот с надписью\n' +
     '/cat hat/says/hello - кот с тегом hat и надписью\n' +
     '/cat gif            - анимированный кот\n' + 
-    'Подробности в https://cataas.com/#/about'
+    '/cat says/hello?color=orange  - кот с надписью оранжевым цветом\n' +
+    '/cat says/aloha?color=red&filter=sepia  - кот с надписью красным цветом в сепии\n' +
+    '   доступны опции:\n' +
+    '   color - цвет текста;\n' +
+    '   size - размер шрифта;\n' +
+    '   type - тип картинки (small, medium, square, original);\n' +
+    '   filter - фильтр (blur,mono,sepia,negative,paint,pixel);\n' +
+    '   width|height - ширина или высота картинки в пикселях;\n' +
+    'Подробности в https://cataas.com/'
 ));
 
 bot.use(commandArgsMiddleware());
@@ -34,12 +42,39 @@ bot.catch((err) => {
 });
 
 // 'cat' command for my dear Tahi.
+const parser = (str, numb) => {
+    let result = '';
+    let matches = str.match(/^\/(?:(?<tag>\w*)(?:\/))?((?:says\/)(?<says>[^?]*))?(?:\?(?<options>.*))?$/);
+
+    if(!matches) return result + "?" + numb;
+
+    if(matches['groups']['tag']) result += '/' + matches['groups']['tag'];
+    if(matches['groups']['says']) result += '/says/'+matches['groups']['says'];
+
+    result += "?" + numb;
+    if(matches['groups']['options']) {
+        let option;
+        option = matches['groups']['options'].match(/(?:s(?:ize)=([\d]+))/);
+        if(option) result += "&ssize=" + option[1];
+        option = matches['groups']['options'].match(/(?:c(?:olor)=([\w]+))/);
+        if(option) result += "&color=" + option[1];
+        option = matches['groups']['options'].match(/(?:t(?:ype)=(sm(all)?|m(e)?d(ium)?|sq(uare)?|or(iginal)?))/);
+        if(option) result += "&type=" + option[1];
+        option = matches['groups']['options'].match(/(?:fi(?:lter)=(blur|mono|sepia|negative|paint|pixel))/);
+        if(option) result += "&filter=" + option[1];
+        option = matches['groups']['options'].match(/(?:wi(?:dth)=([\d]+))/);
+        if(option) result += "&width=" + option[1];
+        option = matches['groups']['options'].match(/(?:he(?:ight)=([\d]+))/);
+        if(option) result += "&height=" + option[1];
+    } 
+    return result;
+};
 const random = (min, max) => Math.floor(Math.random() * (max - min) + min);
 const replacer = (match, p1, p2) => { if(p1) return p1; if(p2) return '%3F'; };
-const encode = (args) => (typeof args === 'string') ? encodeURI(args.replace(/^\/?/, '/').replace(/(\?\w*=\w*)|(\?)/g, replacer)) : '';
+const encode = (args) => (typeof args === 'string') ? encodeURI(args.replace(/^\/?/, '/')).replace(/(\?\w*=\w*)|(\?)/g, replacer) : '';
 bot.command('cat', async (ctx) => {
     const args = ctx.state.command.args;
-    const request = 'https://cataas.com/cat' + encode(args) + '?' + random(1, 1000); 
+    const request = 'https://cataas.com/cat' + parser(encode(args), random(1, 1000)); 
     const response = await fetch(request);
 
     if (response.ok) {
