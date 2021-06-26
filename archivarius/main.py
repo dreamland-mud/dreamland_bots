2a476d46ab44ba92f5768f31c220488c09a8965f
lymfay <86461175+januaryvision@users.noreply.github.com>
2021-06-26 12:37:34 +0300

    @januaryvision
    some upgrades

--- archivarius/main.py
+++ archivarius/main.py
@@ -8,19 +8,11 @@ from aiogram.dispatcher import FSMContext
 from aiogram.dispatcher.filters.state import State, StatesGroup
 
 
-class UserState(StatesGroup):
-    TYPO = State()
-    BUG = State()
-    IDEA = State()
-    NOHELP = State()
-    # user_data = await state.get_data()
-    # await state.finish()
-
-
 logging.basicConfig(level=logging.INFO)
-#bot = Bot(token='1740013764:AAEBYxpTnKgUpebaIog7Ow7qhDXEBhaZZqY')
+# bot = Bot(token='1740013764:AAEBYxpTnKgUpebaIog7Ow7qhDXEBhaZZqY')
 bot = Bot(token='1055339561:AAHZBHX6bzm75nZAOOA1V5fwGsCYCz9ojxY')
 dp = Dispatcher(bot, storage=MemoryStorage())
+
 
 @dp.message_handler(commands=['start'])
 async def welcome(message: types.Message):
@@ -30,68 +22,13 @@ async def welcome(message: types.Message):
 
 
 @dp.message_handler(commands=['help'])
-async def showcommand(message: types.Message):
-    mess = '/help - выведет это сообщение;\n/typo - отправить отчет об ошибке/опечатке;\n' \
-           '/bug - отправить отчет о баге;\n/idea - предложить идею;\n' \
-           '/nohelp - отправить отчет об отсутсвии раздела справки.\n\n' \
-           '<i>!typo текст</i> - отправить отчет об ошибке/опечатке;\n' \
+async def show_command(message: types.Message):
+    mess = '<i>!typo текст</i> - отправить отчет об ошибке/опечатке;\n' \
            '<i>!bug текст</i> - отправить отчет о баге;\n' \
            '<i>!idea текст</i> - предложить идею;\n' \
            '<i>!nohelp текст</i> - отправить отчет об отсутсвии раздела справки.\n\n' \
            'Чтобы посмотреть любой раздел справки из бота - отправь текст формата: ?название_статьи'
     await bot.send_message(message.chat.id, mess, parse_mode='html')
-
-
-@dp.message_handler(commands=['typo'])
-async def typo(message: types.Message):
-    await bot.send_message(message.chat.id, 'О какой именно опечатке ты хочешь сообщить?')
-    await UserState.TYPO.set()
-
-
-@dp.message_handler(commands=['bug'])
-async def typo(message: types.Message):
-    await bot.send_message(message.chat.id, 'О какой именно ошибке ты хочешь сообщить?')
-    await UserState.BUG.set()
-
-
-@dp.message_handler(commands=['idea'])
-async def typo(message: types.Message):
-    await bot.send_message(message.chat.id, 'О какой именно идее ты хочешь сообщить?')
-    await UserState.IDEA.set()
-
-
-@dp.message_handler(commands=['nohelp'])
-async def typo(message: types.Message):
-    await bot.send_message(message.chat.id, 'Об отсутствии какого раздела справки ты хочешь сообщить?')
-    await UserState.NOHELP.set()
-
-async def send(message, cmd):
-    msg = message.text
-    url = dl.endpoint(cmd)
-    author = message.from_user.id
-    response = requests.post(url, data=dl.payload(author, msg))
-    await bot.send_message(message.chat.id, response)
-
-
-@dp.message_handler(state=UserState.TYPO)
-async def send_typo(message, state: FSMContext):
-    await send(message, 'typo')
-    await state.finish()
-
-@dp.message_handler(state=UserState.BUG)
-async def send_bug(message, state: FSMContext):
-    await send(message, 'bug')
-    await state.finish()
-
-@dp.message_handler(state=UserState.IDEA)
-async def send_idea(message, state: FSMContext):
-    await send(message, 'idea')
-    await state.finish()
-
-@dp.message_handler(state=UserState.NOHELP)
-async def send_typo(message, state: FSMContext):
-    await send(message, 'nohelp')
-    await state.finish()
 
 
 @dp.message_handler()
@@ -110,7 +47,10 @@ async def allmsg(message: types.Message):
         await bot.send_message(message.chat.id, response)
 
     elif user_data.startswith('?'):
-        await bot.send_message(message.chat.id, 'Функционал справочника находится в разработке..')
+        url = dl.endpoint('skill')
+        new_user_data = user_data.replace('?', '')
+        response = requests.request(url, data=dl.desc(new_user_data))
+        await bot.send_message(message.chat.id, response)
     else:
         pass
 
