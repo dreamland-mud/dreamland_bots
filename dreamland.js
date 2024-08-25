e7ac4bb19b8a1c4ce960a6871d7e3eb43a85d3a6
Aleksandr Shereshkov <101018177+AleksandrSherehkov@users.noreply.github.com>
2024-08-25 15:36:22 +0300

    feat: Extend /who command to accept "player name" parameter for Telegram bot

--- dreamland.js
+++ dreamland.js
@@ -3,99 +3,142 @@ const api = 'https://dreamland.rocks/api';
 
 let toDream = Promise.resolve();
 
-const enqueueToDream = (url, options={}) => {
-    const chain = async (prev) => {
-        try {
-            await prev;
-            const response = await fetch(url, options);
-            return response;
-
-        } catch (error) {
-            console.log('DreamLand error', error);
-        }
+const enqueueToDream = (url, options = {}) => {
+  const chain = async prev => {
+    try {
+      await prev;
+      const response = await fetch(url, options);
+      return response;
+    } catch (error) {
+      console.log('DreamLand error', error);
     }
+  };
 
-    toDream = chain(toDream);
-    return toDream;
+  toDream = chain(toDream);
+  return toDream;
 };
 
 class DreamLand {
-    constructor(bottype) {
-        this.bottype = bottype;
-        this.token = process.env.DREAMLAND_TOKEN;
-    }
-    
-    body(args) {
-        return {args, token: this.token, bottype: this.bottype};
-    }
+  constructor(bottype) {
+    this.bottype = bottype;
+    this.token = process.env.DREAMLAND_TOKEN;
+  }
 
-    options(args) {
-        return {method: 'POST', body: JSON.stringify(this.body(args))};
-    }
-    
-    async who() {
-        const response = await enqueueToDream(`${api}/who`);
-        const who = await response.json();
-        let result = '';
+  body(args) {
+    return { args, token: this.token, bottype: this.bottype };
+  }
 
-        if (who.total === 0) {
-            result = 'В мире никого нет!';
-        } else {
-	    result = '```';
-            if (who.people && who.people.length > 0)
-                result += '\nСейчас в мире:\n\n'
-                       + who.people
-                           .map(p =>
-                                (p.name.ru || p.name.en) + ', ' + p.race.ru + (p.clan ? ', клан ' + p.clan.en : '')
-                           ).join('\n');
+  options(args) {
+    return { method: 'POST', body: JSON.stringify(this.body(args)) };
+  }
 
-            if (who.discord && who.discord.length > 0)
-                result += '\n\nСлышат каналы: '
-                          + who.discord.map(p => (p.name.ru || p.name.en)).join(', ');
+  async who() {
+    const args = { message: '' };
+    const response = await enqueueToDream(`${api}/who`, this.options(args));
+    const who = await response.json();
 
-            result += '\n\nВсего игроков: ' + who.total + '.\n```';
-        }
+    let result = '';
 
-        return result;
-    }
+    if (who.total === 0) {
+      result = 'В мире никого нет!';
+    } else {
+      result = '```';
+      if (who.people && who.people.length > 0)
+        result +=
+          '\nСейчас в мире:\n\n' +
+          who.people
+            .map(
+              p =>
+                (p.name.ru || p.name.en) +
+                ', ' +
+                p.race.ru +
+                (p.clan ? ', клан ' + p.clan.en : '')
+            )
+            .join('\n');
 
+      if (who.discord && who.discord.length > 0)
+        result +=
+          '\n\nСлышат каналы: ' +
+          who.discord.map(p => p.name.ru || p.name.en).join(', ');
 
-    async ooc(args) {
-        const response = await enqueueToDream(`${api}/ooc`, this.options(args));
-        let result
-
-        if (response.ok)
-            result = undefined
-        else if (response.status === 404)
-            result = 'Ты не присоединен ни к одному персонажу. Зайди в DreamLand и набери config ' + this.bottype + '.'
-        else
-            result = 'Произошла ошибка, попробуй позже.'
-
-        return result
+      result += '\n\nВсего игроков: ' + who.total + '.\n```';
     }
 
+    return result;
+  }
 
-    async link(args) {
-        const response = await enqueueToDream(`${api}/link`, this.options(args));
-        let result
+  async whois(playerName) {
+    const args = { message: playerName };
+    const response = await enqueueToDream(`${api}/whois`, this.options(args));
 
-        if (response.ok)
-            result = 'Успешно присоединен к персонажу.'
-        else if (response.status === 404)
-            result = 'Персонажа с таким секретным словом не существует. Зайди в DreamLand и набери config ' + this.bottype + '.'
-        else
-            result = 'Произошла ошибка, попробуй позже.'
+    const whoisData = await response.json();
 
-        return result
+    let result = '';
+
+    if (response.ok && !whoisData.error) {
+      const name = whoisData.name?.ru || whoisData.name?.en || playerName;
+      const race = whoisData.race || 'Неизвестно';
+      const clan = whoisData.clan ? whoisData.clan.name : 'Нет';
+      const remorts = whoisData.remorts || '0';
+      const title = whoisData.clan?.title
+        ? `*Титул у клані:* ${whoisData.clan.title}\n`
+        : '';
+      const capitalizedPlayerName =
+        name.charAt(0).toUpperCase() + name.slice(1);
+
+      result =
+        `*Інформація про гравця:*\n\n` +
+        `*Ім'я:* ${capitalizedPlayerName}\n` +
+        `*Раса:* ${race}\n` +
+        `*Клан:* ${clan}\n` +
+        title +
+        `*Кількість перероджень:* ${remorts}`;
+    } else if (whoisData.error === 'player not found') {
+      result = `Персонаж із таким ім'ям не знайдений`;
+    } else {
+      result = 'Сталася помилка, спробуйте пізніше.';
     }
 
-    async updateAll(args) {
-        await enqueueToDream(`${api}/update/all`, this.options(args));
-    }
+    return result;
+  }
 
-    async updateOne(args) {
-        await enqueueToDream(`${api}/update/one`, this.options(args));
-    }
+  async ooc(args) {
+    const response = await enqueueToDream(`${api}/ooc`, this.options(args));
+    let result;
+
+    if (response.ok) result = undefined;
+    else if (response.status === 404)
+      result =
+        'Ты не присоединен ни к одному персонажу. Зайди в DreamLand и набери config ' +
+        this.bottype +
+        '.';
+    else result = 'Произошла ошибка, попробуй позже.';
+
+    return result;
+  }
+
+  async link(args) {
+    const response = await enqueueToDream(`${api}/link`, this.options(args));
+    let result;
+
+    if (response.ok) result = 'Успешно присоединен к персонажу.';
+    else if (response.status === 404)
+      result =
+        'Персонажа с таким секретным словом не существует. Зайди в DreamLand и набери config ' +
+        this.bottype +
+        '.';
+    else result = 'Произошла ошибка, попробуй позже.';
+
+    return result;
+  }
+
+  async updateAll(args) {
+    await enqueueToDream(`${api}/update/all`, this.options(args));
+  }
+
+  async updateOne(args) {
+    await enqueueToDream(`${api}/update/one`, this.options(args));
+  }
 }
 
 module.exports = DreamLand;
--- telegram/index.js
+++ telegram/index.js
@@ -7,88 +7,122 @@ const dreamland = new DreamLand('telegram');
 const commandArgsMiddleware = require('./commandArgs');
 const fetch = require('node-fetch');
 
-
 // 'start' - standard bot command
-bot.start((ctx) => ctx.reply('Привет, я Хассан и я скоро поумнею. Набери /help для списка команд.'));
+bot.start(ctx =>
+  ctx.reply(
+    'Привет, я Хассан и я скоро поумнею. Набери /help для списка команд.'
+  )
+);
 
 // 'help' command, can be reworked to show action buttons
-bot.help((ctx) => ctx.reply(
-    '/who                - показать кто в мире\n' + 
-    '/cat                - случайный котик\n' +
-    '/cat says/meow meow - кот с надписью\n' +
-    '/cat hat/says/hello - кот с тегом hat и надписью\n' +
-    '/cat gif            - анимированный кот\n' + 
-    '/cat says/hello?color=orange  - кот с надписью оранжевым цветом\n' +
-    '/cat says/aloha?color=red&filter=sepia  - кот с надписью красным цветом в сепии\n' +
-    '   доступны опции:\n' +
-    '   color - цвет текста;\n' +
-    '   size - размер шрифта;\n' +
-    '   type - тип картинки (small, medium, square, original);\n' +
-    '   filter - фильтр (blur,mono,sepia,negative,paint,pixel);\n' +
-    '   width|height - ширина или высота картинки в пикселях;\n' +
-    'Подробности в https://cataas.com/'
-));
+bot.help(ctx =>
+  ctx.reply(
+    '/who                - показать кто в мире\n' +
+      '/cat                - случайный котик\n' +
+      '/cat says/meow meow - кот с надписью\n' +
+      '/cat hat/says/hello - кот с тегом hat и надписью\n' +
+      '/cat gif            - анимированный кот\n' +
+      '/cat says/hello?color=orange  - кот с надписью оранжевым цветом\n' +
+      '/cat says/aloha?color=red&filter=sepia  - кот с надписью красным цветом в сепии\n' +
+      '   доступны опции:\n' +
+      '   color - цвет текста;\n' +
+      '   size - размер шрифта;\n' +
+      '   type - тип картинки (small, medium, square, original);\n' +
+      '   filter - фильтр (blur,mono,sepia,negative,paint,pixel);\n' +
+      '   width|height - ширина или высота картинки в пикселях;\n' +
+      'Подробности в https://cataas.com/'
+  )
+);
 
 bot.use(commandArgsMiddleware());
 
-// 'who' command using DL API, anyone can call.
-bot.command('who', async (ctx) => {
-    const response = await dreamland.who();
+bot.command('who', async ctx => {
+  const args = { message: ctx.state.command.args };
+
+  let response;
+
+  try {
+    if (args.message.length > 0) {
+      response = await dreamland.whois(args.message);
+    } else {
+      response = await dreamland.who();
+    }
+
     ctx.replyWithMarkdown(response);
+  } catch (error) {
+    console.error('Ошибка при выполнении команды /who:', error);
+    ctx.reply(
+      'Произошла ошибка при получении информации. Пожалуйста, попробуйте позже.'
+    );
+  }
 });
 
-bot.catch((err) => {
-    console.log(err);
+bot.catch(err => {
+  console.log(err);
 });
 
 // 'cat' command for my dear Tahi.
 const parser = (str, numb) => {
-    let result = '';
-    let matches = str.match(/^\/(?:(?<tag>\w*)(?:\/)?)?((?:says\/)(?<says>[^?]*))?(?:\?(?<options>.*))?$/);
+  let result = '';
+  let matches = str.match(
+    /^\/(?:(?<tag>\w*)(?:\/)?)?((?:says\/)(?<says>[^?]*))?(?:\?(?<options>.*))?$/
+  );
 
-    if(!matches) return result + "?" + numb;
+  if (!matches) return result + '?' + numb;
 
-    if(matches['groups']['tag']) result += '/' + matches['groups']['tag'];
-    if(matches['groups']['says']) result += '/says/'+matches['groups']['says'];
+  if (matches['groups']['tag']) result += '/' + matches['groups']['tag'];
+  if (matches['groups']['says']) result += '/says/' + matches['groups']['says'];
 
-    result += "?" + numb;
-    if(matches['groups']['options']) {
-        let option;
-        option = matches['groups']['options'].match(/(?:s(?:ize)=([\d]+))/);
-        if(option) result += "&size=" + option[1];
-        option = matches['groups']['options'].match(/(?:c(?:olor)=([\w]+))/);
-        if(option) result += "&color=" + option[1];
-        option = matches['groups']['options'].match(/(?:t(?:ype)=(sm(all)?|m(e)?d(ium)?|sq(uare)?|or(iginal)?))/);
-        if(option) result += "&type=" + option[1];
-        option = matches['groups']['options'].match(/(?:fi(?:lter)=(blur|mono|sepia|negative|paint|pixel))/);
-        if(option) result += "&filter=" + option[1];
-        option = matches['groups']['options'].match(/(?:wi(?:dth)=([\d]+))/);
-        if(option) result += "&width=" + option[1];
-        option = matches['groups']['options'].match(/(?:he(?:ight)=([\d]+))/);
-        if(option) result += "&height=" + option[1];
-    } 
-    return result;
+  result += '?' + numb;
+  if (matches['groups']['options']) {
+    let option;
+    option = matches['groups']['options'].match(/(?:s(?:ize)=([\d]+))/);
+    if (option) result += '&size=' + option[1];
+    option = matches['groups']['options'].match(/(?:c(?:olor)=([\w]+))/);
+    if (option) result += '&color=' + option[1];
+    option = matches['groups']['options'].match(
+      /(?:t(?:ype)=(sm(all)?|m(e)?d(ium)?|sq(uare)?|or(iginal)?))/
+    );
+    if (option) result += '&type=' + option[1];
+    option = matches['groups']['options'].match(
+      /(?:fi(?:lter)=(blur|mono|sepia|negative|paint|pixel))/
+    );
+    if (option) result += '&filter=' + option[1];
+    option = matches['groups']['options'].match(/(?:wi(?:dth)=([\d]+))/);
+    if (option) result += '&width=' + option[1];
+    option = matches['groups']['options'].match(/(?:he(?:ight)=([\d]+))/);
+    if (option) result += '&height=' + option[1];
+  }
+  return result;
 };
 const random = (min, max) => Math.floor(Math.random() * (max - min) + min);
-const replacer = (match, p1, p2) => { if(p1) return p1; if(p2) return '%3F'; };
-const encode = (args) => (typeof args === 'string') ? encodeURI(args.replace(/^\/?/, '/')).replace(/(\?\w*=\w*)|(\?)/g, replacer) : '';
-bot.command('cat', async (ctx) => {
-    const args = ctx.state.command.args;
-    const request = 'https://cataas.com/cat' + parser(encode(args), random(1, 1000)); 
-    const response = await fetch(request);
+const replacer = (match, p1, p2) => {
+  if (p1) return p1;
+  if (p2) return '%3F';
+};
+const encode = args =>
+  typeof args === 'string'
+    ? encodeURI(args.replace(/^\/?/, '/')).replace(
+        /(\?\w*=\w*)|(\?)/g,
+        replacer
+      )
+    : '';
+bot.command('cat', async ctx => {
+  const args = ctx.state.command.args;
+  const request =
+    'https://cataas.com/cat' + parser(encode(args), random(1, 1000));
+  const response = await fetch(request);
 
-    if (response.ok) {
-        if (response.headers.get('content-type') === 'image/gif')
-            return ctx.replyWithDocument(request);
-        else
-            return ctx.replyWithPhoto(request);
-    }
+  if (response.ok) {
+    if (response.headers.get('content-type') === 'image/gif')
+      return ctx.replyWithDocument(request);
+    else return ctx.replyWithPhoto(request);
+  }
 
-    if (response.status === 404)
-        return ctx.reply('Неправильный запрос, читайте /help');
+  if (response.status === 404)
+    return ctx.reply('Неправильный запрос, читайте /help');
 
-    return ctx.reply('Какая-то ошибка, попробуйте позже.');
+  return ctx.reply('Какая-то ошибка, попробуйте позже.');
 });
-
 
 bot.launch();
